c01af280c084db26c7e08fe6ed196b644699d051
Raquel Lima <lima@adobe.com>
2024-03-13 17:59:18 +0100

    add editables

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app-title" data-aue-prop="title" data-aue-type="text">
+        Test App
+      </h1>
+      <h2
+        className="app-descrition-title"
+        data-aue-prop="title"
+        data-aue-type="text"
+      >
+        Description
+      </h2>
+      <div
+        className="app-description"
+        data-aue-prop="description"
+        data-aue-type="richtext"
+      >
+        This is a test app
+      </div>
     </div>
   );
 }
