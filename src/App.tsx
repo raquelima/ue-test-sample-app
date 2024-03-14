27bdf9447adb8db40accb361fe01f41e723c6584
Raquel Lima <lima@adobe.com>
2024-03-14 10:59:07 +0100

    add more editables

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
 function App() {
   return (
-    <div className="app">
-      <h1 className="app-title" data-aue-prop="title" data-aue-type="text">
+    <div
+      data-aue-resource="urn:test:/content/test-app/container"
+      data-aue-type="container"
+    >
+      <h1
+        data-aue-resource="urn:test:/content/test-app/text"
+        data-aue-prop="title"
+        data-aue-type="text"
+        data-aue-behavior="component"
+      >
         Test App
       </h1>
       <h2
-        className="app-descrition-title"
+        data-aue-resource="urn:test:/content/test-app/text"
         data-aue-prop="title"
         data-aue-type="text"
+        data-aue-behavior="component"
       >
         Description
       </h2>
       <div
-        className="app-description"
+        data-aue-resource="urn:test:/content/test-app/text"
         data-aue-prop="description"
         data-aue-type="richtext"
+        data-aue-behavior="component"
       >
         This is a test app
       </div>
+      <ul
+        data-aue-resource="urn:test:/content/test-app/list"
+        data-aue-type="container"
+      >
+        <li
+          data-aue-resource="urn:test:/content/test-app/listitem"
+          data-aue-prop="text"
+          data-aue-type="text"
+          data-aue-behavior="component"
+        >
+          List Item 1
+        </li>
+
+        <li
+          data-aue-resource="urn:test:/content/test-app/listitem"
+          data-aue-prop="text"
+          data-aue-type="text"
+          data-aue-behavior="component"
+        >
+          List Item 2
+        </li>
+      </ul>
     </div>
   );
 }
